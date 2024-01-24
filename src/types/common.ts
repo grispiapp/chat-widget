81019babf50673732f660e162438cb0fe35f564d
Davut Kember <davutkmbr@gmail.com>
2024-01-24 20:23:51 +0300

    move chat options type

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,4 +1,3 @@
-import { ENVIRONMENTS } from "@lib/config";
 import { GrispiChat } from "..";
 
 declare global {
@@ -6,19 +5,4 @@ declare global {
     GrispiChat: typeof GrispiChat;
     GRISPI_CHAT_JS_VERSION: string;
   }
-}
-
-export interface GrispiChatOptions {
-  environment?: (typeof ENVIRONMENTS)[keyof typeof ENVIRONMENTS];
-  debug?: boolean;
-  element?: string;
-  colors: {
-    primary: string;
-  };
-  agent: {
-    name: string;
-    avatar: string;
-  };
-  welcome_message: string;
-  popup_message: string;
 }
