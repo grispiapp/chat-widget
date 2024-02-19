ce7ea78f26d9cca14319bc4a08cf38af4e85672c
Davut Kember <davutkmbr@gmail.com>
2024-02-19 16:25:21 +0300

    fix assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,26 @@
 import preact from "@preact/preset-vite";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [preact()],
-    resolve: {
-        alias: {
-            "@components": path.resolve(__dirname, "./src/components"),
-            "@ui": path.resolve(__dirname, "./src/components/common/ui"),
-            "@lib": path.resolve(__dirname, "./src/lib"),
-            "@hooks": path.resolve(__dirname, "./src/hooks"),
-            "@context": path.resolve(__dirname, "./src/context"),
-            "@resources": path.resolve(__dirname, "./resources"),
-            "@": path.resolve(__dirname, "./src"),
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+    return defineConfig({
+        plugins: [preact()],
+        resolve: {
+            alias: {
+                "@components": path.resolve(__dirname, "./src/components"),
+                "@ui": path.resolve(__dirname, "./src/components/common/ui"),
+                "@lib": path.resolve(__dirname, "./src/lib"),
+                "@hooks": path.resolve(__dirname, "./src/hooks"),
+                "@context": path.resolve(__dirname, "./src/context"),
+                "@resources": path.resolve(__dirname, "./resources"),
+                "@": path.resolve(__dirname, "./src"),
+            },
         },
-    },
-});
+        server: {
+            origin: process.env.VITE_APP_URL,
+        },
+    });
+};
